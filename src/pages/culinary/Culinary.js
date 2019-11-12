88fcc17ea66d1bcf9a3ecfe4909f3321dc3a67d9
nanihutagaol <nanihutagaol@gmail.com>
2019-11-12 19:56:31 +0700

    modified culinary pages

--- src/pages/culinary/Culinary.js
+++ src/pages/culinary/Culinary.js
@@ -1,27 +1,66 @@
-// import { mapGetters } from 'vuex'
+import { mapGetters } from 'vuex'
 import BannerCategory from '@/components/BannerCategory'
+import FilterCulinary from '@/components/FilterCulinary'
+import SortCulinary from '@/components/SortCulinary'
 import CardCulinary from '@/components/CardCulinary'
 
 export default {
-  name: 'CulinaryAdmin',
+  name: 'Culinary',
   components: {
     BannerCategory,
+    FilterCulinary,
+    SortCulinary,
     CardCulinary
   },
   data () {
-    return {}
+    return {
+    }
   },
   created () {
-    this.$store.dispatch('getCulinary')
+    this.$store.dispatch('getRestaurantList')
   },
   computed: {
-    culinaryList: function () {
-      return this.$store.getters.culinaryList
+    ...mapGetters([
+      'restaurantList', 'restaurantFiltered', 'isFilterActive'
+    ]),
+    filteredRestaurantList () {
+      if (this.isFilterActive === true){
+        return this.restaurantFiltered
+      } else {
+        return this.restaurantList
+      }
     }
-    // ...mapGetters('culinary', [
-    //   'culinaryList'
-    // ])
   },
   methods: {
+    onFilterByName (selectedName) {
+      let restaurantList = [...this.restaurantList]
+      let restaurants = this.filterByRestaurantName(restaurantList, selectedName)
+      this.$store.dispatch('onSetRestaurantFiltered', restaurants)
+    },
+    filterByRestaurantName (restaurantList, selectedName) {
+      let out = [];
+
+      restaurantList.forEach(function (restaurant) {
+        selectedName.forEach(function (selected) {
+          if (selected && selected.isActive === true && selected.restaurantId === restaurant.restaurantId) {
+            out.push(restaurant);
+            return;
+          }
+        });
+      })
+
+      return out;
+    },
+    onSortBy(criteria) {
+      console.log(criteria)
+    },
+    sortByPrice (restaurantList, type) {
+      return restaurantList.sort((a, b) => {
+        if (type === 'asc')
+          return a.offerFare - b.offerFare
+        else
+          return b.offerFare - a.offerFare
+      })
+    },
   }
 }
--- src/pages/culinary/index.vue
+++ src/pages/culinary/index.vue
@@ -1,25 +1,61 @@
 <template>
-<div class="culinary-by-admin">
-  <BannerCategory/>
+  <div class="culinary-by-admin">
+    <BannerCategory/>
 
-  <div class="container">
-    <div class="culinary-title-group">
-      <h1>
-        Kuliner Populer
-      </h1>
-      <div class="culinary-add-button">
-        <button class="btn btn-success">Tambah</button>
-      </div>
-    </div>
-    <div class="row list-culinary-group">
-      <div class="col-sm-3 list-culinary" v-for="culinary in culinaryList" :key="culinary">
-        <CardCulinary/>
+    <div class="container">
+      <div class="row">
+        <div class="culinary-row">
+          <h1>
+            Kuliner Populer
+          </h1>
+        </div>
+
+        <div class="culinary-row culinary-add-button">
+          <div class="row">
+            <div class="col-sm-3">
+            </div>
+
+            <div class="col-sm-9">
+              <button class="btn btn-success"><i class="fa fa-plus"></i> Tambah</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="culinary-row">
+          <div class="row">
+            <div class="col-sm-3">
+              <div style="margin-bottom: 1em">
+                <SortCulinary
+                  @onSortBy="onSortBy"
+                />
+              </div>
+
+              <div>
+                <FilterCulinary
+                  :restaurantList="restaurantList"
+                  @onFilterByName="onFilterByName"
+                />
+              </div>
+            </div>
+
+            <div class="col-sm-9">
+              <div class="row" v-for="restaurant in filteredRestaurantList" :key="restaurant.restaurantId">
+                <div class="col-sm-4 food-cart" v-for="food in restaurant.foodList" :key="food.foodId">
+                  <div class="">
+                    <CardCulinary :food="food"/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
       </div>
     </div>
   </div>
-</div>
 </template>
 
-<script src="./CulinaryAdmin.js"></script>
+<script src="./Culinary.js"></script>
 
-<style src="./CulinaryAdmin.css" scoped></style>
+<style src="./Culinary.css" scoped></style>
